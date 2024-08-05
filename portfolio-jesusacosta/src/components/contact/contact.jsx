7ab513265a0f9bbfbe5872a9608d1edd3a52f329
Jessuss <jesusacosta3321@gmail.com>
2024-08-05 11:06:13 -0300

    fixs

--- portfolio-jesusacosta/src/components/contact/contact.jsx
+++ portfolio-jesusacosta/src/components/contact/contact.jsx
@@ -3,9 +3,7 @@ export default function Contact() {
         <div className="min-h-screen">
             <h2 className="text-[33px] text-center font-black mt-20">
           <span className=" text-blue-900">#</span>Contact
-          <span className="h-[1px] md:ml-6 w-full md:w-[450px] inline-block bg-light relative top-7 group-hover:w-full transition-[width] ease duration-300 bg-white">
-            &nbsp;
-          </span>
+          
         </h2>
         </div>
     );
--- portfolio-jesusacosta/src/components/experience/experience.jsx
+++ portfolio-jesusacosta/src/components/experience/experience.jsx
@@ -3,9 +3,6 @@ export default function Experience() {
     <div className="min-h-screen">
       <h2 className="text-[33px] text-center font-black mt-20">
         <span className=" text-blue-900">#</span>Experience
-        <span className="h-[1px] md:ml-6 w-full md:w-[450px] inline-block bg-light relative top-7 group-hover:w-full transition-[width] ease duration-300 bg-white">
-          &nbsp;
-        </span>
       </h2>
       <p>
         I have experience in group projects using agile methodologies and
@@ -17,7 +14,7 @@ export default function Experience() {
       <div>
         <span>
           More that 1 years experience as a Full Stack Web Developer and 5
-          Projects Completed .
+          Projects Completed.
         </span>
       </div>
     </div>
